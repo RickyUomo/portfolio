61968376be0751242c235c41817ddcf95b882d60
Ricky Kuo <rickykuo925@gmail.com>
2021-10-12 11:47:48 +0100

    add netflix project

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,5 @@
 import p1 from './img/p_1.gif'
+import netflix from './img/netflix.png'
 
 export const projects = [
     {
@@ -7,5 +8,11 @@ export const projects = [
             "React and Django REST framework application to predict infant's mortality",
         image: p1,
         link: "https://infant-front.herokuapp.com/",
+    },{
+        title: 'Netflix Clone',
+        description:
+            "React, styled components and firebase to rebuild Netflix interface. However, still under construction",
+        image: netflix,
+        link: "https://netflixc-clone.herokuapp.com/",
     }
 ]
