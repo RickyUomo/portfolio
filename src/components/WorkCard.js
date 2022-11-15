a51e302a656560356b7462f6243647ab95a2e8a0
Ricky Kuo <rickykuo925@gmail.com>
2022-11-15 22:31:42 +0000

    work card

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,14 +5,18 @@ export default function WorkCard({ work }) {
 
     return (
         <div className="pr-14 pl-14 pt-8 pb-8 mt-8 mb-8 bg-blue-900 bg-opacity-25">
-            <header className="mb-6">
-                <h1 className="text-2xl">{work.title} at {work.company}</h1>
-                <h1 className="">{work.time}</h1>
-            </header>
+            <div>
+                <header className="mb-6">
+                    <p class="mb-3 text-base text-yellow-300">{work.time}</p>
+                    <h1 className="text-2xl">{work.title} at {work.company}</h1>
+                </header>
 
-            <ul className="list-disc">
-                {desc.map(desc => <li>{desc}</li>)}
-            </ul>
+                <div>
+                    <ul className="list-disc">
+                        {desc.map(desc => <li>{desc}</li>)}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
