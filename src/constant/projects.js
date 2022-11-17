f36ddb5f7b31cf8b5c7897f478c89a48ad8edeeb
Ricky Kuo <rickykuo925@gmail.com>
2022-11-17 17:40:42 +0000

    url

--- src/constant/projects.js
+++ src/constant/projects.js
@@ -22,7 +22,7 @@ export const projects = [
         description:
             "Responsively movie searching application, implemented debounce to restrict the amount of API calls. <React, TMDB>",
         image: movie,
-        link: 'https://mengju-movie-search.netlify.app/discover'
+        link: 'https://mengju-movie-search.netlify.app/'
     },
     {
         title: 'Rib Responsive',
