24b2b54db6f16f853fcaf510cfd31dc00a4e8f8f
Ricky Kuo <rickykuo925@gmail.com>
2023-01-08 18:35:28 +0000

    set up blog components

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,29 @@
 import React from "react";
+import PostCard from "./PostCard";
+import Categories from "./Categories";
+import PostWidget from "./PostWidget";
+import { useEffect } from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  useEffect(() => {});
+
+  return (
+    <div className="container max-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {/* {posts.map((post) => (
+            <PostCard key={post.title} post={post} />
+          ))} */}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
