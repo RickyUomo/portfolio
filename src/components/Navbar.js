0449e16e7ba6f21de086837f318014568a5f160d
Ricky Kuo <rickykuo925@gmail.com>
2023-03-13 10:20:27 +0000

    remove Link import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 
 export default function Navbar() {
